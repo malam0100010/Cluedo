344b81520694446d258b65b63674ea6b77b0bb9c
valortbuck <valor_buck@mines.edu>
2025-02-27 17:41:40 -0700

    drvier: Valor created test classes

---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,18 @@
+package experiment;
+
+public class TestBoardCell {
+	private int row;
+	private int col;
+	
+
+	TestBoardCell(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	//setter to add a cell to this cells adjacency list
+	void addAdjacency(TestBoardCell cell) {
+		
+	}
+	
+}
