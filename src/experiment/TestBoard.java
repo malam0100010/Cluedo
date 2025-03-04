a9ea119f4e20be04f14c20fd31cde564fe1da615
valortbuck <valor_buck@mines.edu>
2025-03-03 17:15:26 -0700

    for submittal

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -10,22 +10,22 @@ public class TestBoard
     TestBoardCell testBoardCell;
     Set<TestBoardCell> targets;
 
-    TestBoard()
+    public TestBoard()
     {
 
     }
 
-    void calcTargets( TestBoardCell startCell, int pathlength)
+    public void calcTargets( TestBoardCell startCell, int pathlength)
     {
         return;
     }
 
-    TestBoardCell getCell(int row, int col)
+    public TestBoardCell getCell(int row, int col)
     {
         return testBoardCell;
     }
 
-    Set<TestBoardCell> getTargets()
+    public Set<TestBoardCell> getTargets()
     {
         return targets;
     }
